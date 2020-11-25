08860fad675b49933e2763fd5624cde5d0e4e9d3
Konstantin <29775482+lobr17@users.noreply.github.com>
2020-11-25 23:38:35 +0300

    Update ItemClass.php

--- ItemClass.php
+++ ItemClass.php
@@ -1,38 +1,57 @@
 <?php
 
-final class item {
+final class ItemClass
+{
+    private int $id;
+    private string $name;
+    private int $status;
+    private bool $changed;
 
-    private $id;
-    private $name;
-    private $status;
-    private $changed;
-
-
-    public function __construct($id) {
+    public function __construct($id, $name, $status, $changed)
+    {
         $this->id = $id;
+        $this->init($name, $status, $changed);
     }
 
-    private function init($name, $status) {
-	$this->name = $name;
-	$this->status = $status;
-    }    
-
-    public function __get($name) {	    
-        if (property_exists($this, $name)) {
-		return $this->$name;
-	}
+    private function init($name, $status, $changed)
+    {
+        $this->name = $name;
+        $this->status = $status;
+        $this->changed = $changed;
     }
 
-    public function __set($name, $valueName) {
-        if (property_exists($this, $name) and (is_string($name)) ) {
-	    $this->$name = $valueName;
-	}
-
+    public function __set($property, $value)
+    {
+        switch ($property) {
+            case 'name':
+                if (is_string($value) and isset($value)) {
+                    $this->$property = $value;
+                }
+                break;
+            case 'status':
+                if (is_int($value) and isset($value)) {
+                    $this->$property = $value;
+                }
+                break;
+            case 'changed':
+                if (is_bool($value)) {
+                    $this->$property = $value;
+                }
+                break;
+        }
     }
 
- //???
-    public function save($named, $status) {
-    
+    public function __get($property)
+    {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
     }
 
-}   
+    public function save($name, $status)
+    {
+        $this->name = $name;
+        $this->status = $status;
+    }
+
+}
